ba4544774c7dd9d9f42da80fedb6ad41b4a6d18f
rafaela <rafaelaandrademoraesz@gmail.com>
2021-09-30 11:11:46 -0300

    telas

---
+++ app/src/main/java/com/example/app_findpet/tela_escolha_cadastro.kt
@@ -0,0 +1,11 @@
+package com.example.app_findpet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tela_escolha_cadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_escolha_cadastro)
+    }
+}
