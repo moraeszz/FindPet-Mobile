040aece94cf484f85924a540f2ed464164d2a462
LETICIA BEZERRA COUTINHO SIL <20272564@cfp127aluno.local>
2021-10-05 09:14:13 -0300

    layout tela cadastro instituição pronto

--- app/src/main/java/com/example/app_findpet/tela_cadastro_instituicoes.kt
+++ app/src/main/java/com/example/app_findpet/tela_cadastro_instituicoes.kt
@@ -1,11 +1,39 @@
 package com.example.app_findpet
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.ImageView
 
 class tela_cadastro_instituicoes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_cadastro_instituicoes)
     }
+
+    fun onCheckboxClicked(view: View) {}
 }
+
+//Parte do checkBox
+fun onCheckboxClicked(view: View) {
+    if (view is CheckBox) {
+        val checked: Boolean = view.isChecked
+
+        when (view.id) {
+            R.id.checkbox_meat -> {
+                if (checked) {
+                    // Put some meat on the sandwich
+                } else {
+                    // Remove the meat
+                }
+            }
+        }
+    }
+}
+
+//parte de trocar a imagem
+
